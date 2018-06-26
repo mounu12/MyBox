086333329324e03e1d960e5eb49553d80ad82d53
TejaswiChava <teja.chava4@gmail.com>
2018-06-26 14:21:04 +0530

    header-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CookieService } from 'angular2-cookie/services/cookies.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { FooterComponent } from './components/common/footer/footer.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { LoginComponent } from './components/common/login/login.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent,
-    LoginComponent
+    LoginComponent,
+    AdminComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd  } from '@angular/router';
+import { CookieService } from 'angular2-cookie/core';
 
 @Component({
   selector: 'app-header',
@@ -10,34 +11,53 @@ export class HeaderComponent implements OnInit {
     urlValues: any;
     loginData: any;
     loginStatus: boolean;
-    constructor() { }
+    constructor(private router: Router , private cookieService: CookieService ) { }
 
     ngOnInit() {
+        this.loadLoginData();
         this.loadHeaderChanges();
     }
+    loadLoginData() {
+        this.loginData = this.cookieService.getObject('loginData');
+    }
     loadHeaderChanges() {
-        this.urlValues = [
-            {
-                'urlLink':'/login',
-                'urlName':'Login'
-            },
-            {
-                'urlLink': '/home',
-                'urlName': 'My Box',
-                'icon' : '<i class="fa fa-user sdmcicon"></i>'
-            },
-            {
-                'urlLink': '/about',
-                'urlName': 'Modify Password',
-                'icon' : '<i class="fa fa-edit modifypasswordicon"></i>'
-            },
-            {
-                'urlLink': '/contact',
-                'urlName': 'Logout',
-                'icon' : '<i class="fa fa-power-off logouticon">'
-            },
-        ];
-    } 
-}  
+        if (this.loginData) {
+            if (this.loginData.role === 'admin') {
+                this.loginStatus = true;
+                this.urlValues = [
+                    {
+                        'urlLink': '/home',
+                        'urlName': 'My Box',
+                        'icon' : '<i class="fa fa-user sdmcicon"></i>'
+                    },
+                    {
+                        'urlLink': '/about',
+                        'urlName': 'Modify Password',
+                        'icon' : '<i class="fa fa-edit modifypasswordicon"></i>'
+                    },
+                    // {
+                    //     'urlLink': '/contact',
+                    //     'urlName': 'Logout',
+                    //     'icon' : '<i class="fa fa-power-off logouticon">'
+                    // },
+                ];
+            }
+        } else {
+            this.loginStatus = false;
+            this.urlValues = [
+                {
+                    'urlLink': '/login',
+                    'urlName': 'Login'
+                } ];
+            }
+    }
+    logout() {
+        console.log('logout');
+        this.cookieService.removeAll();
+        this.loadHeaderChanges();
+        window.location.reload();
+        this.router.navigate(['login']);
+    }
+}
 
 
